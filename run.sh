c364f5c7b1c64b222285df83de8921db0c4f19e9
Juneyoung Lee <lebjuney@amazon.com>
2024-02-20 06:41:38 +0000

    update

--- run.sh
+++ run.sh
@@ -14,17 +14,19 @@ REDIRECT_OUTPUT="--log --logdir=${LOG_DIR}"
 #${SLOTHY_DIR}/slothy-cli Arm_AArch64 Arm_Cortex_A55 \
 #    ${CLEAN_DIR}/bignum_mul_8_16_neon.objdump.nostack.s \
 #    -o ${OPT_DIR}/test_nosymvars.s \
-#    -c reserved_regs="[x18]" -c outputs=[hint_buffer32,hint_buffer48,hint_buffer64,hint_buffer80,hint_buffer96,hint_buffer112] \
+#    -c reserved_regs="[x18,sp]" -c outputs=[hint_buffer32,hint_buffer48,hint_buffer64,hint_buffer80,hint_buffer96,hint_buffer112] \
 #    -c /visualize_reordering \
 #    $REDIRECT_OUTPUT
 
 OUTPUTS="[hint_buffer32,hint_buffer48,hint_buffer64,hint_buffer80,hint_buffer96,hint_buffer112]"
 
+if false; then
+
 echo "** Step 0: Resolve symbolic registers"
 ${SLOTHY_DIR}/slothy-cli Arm_AArch64 Arm_Cortex_A55                          \
        ${CLEAN_DIR}/bignum_mul_8_16_neon.objdump.nostack.s                             \
     -o ${OPT_DIR}/bignum_mul_8_16_neon.objdump.nostack_nosymvars.s                     \
-    -c outputs=$OUTPUTS -c reserved_regs="[x18]"         \
+    -c outputs=$OUTPUTS -c reserved_regs="[x18,sp]"         \
     -c constraints.allow_reordering=False                                    \
     -c constraints.functional_only=True                                      \
     -c /visualize_reordering \
@@ -34,7 +36,7 @@ echo "** Step 1: Preprocessing"
 ${SLOTHY_DIR}/slothy-cli Arm_AArch64 Arm_Cortex_A55                          \
        ${OPT_DIR}/bignum_mul_8_16_neon.objdump.nostack_nosymvars.s                     \
     -o ${OPT_DIR}/bignum_mul_8_16_neon.objdump.nostack_unfold_process0.s               \
-    -c outputs=$OUTPUTS -c reserved_regs="[x18]"         \
+    -c outputs=$OUTPUTS -c reserved_regs="[x18,sp]"         \
     -c split_heuristic -c split_heuristic_repeat=0                           \
     -c split_heuristic_preprocess_naive_interleaving                         \
     -c /visualize_reordering \
@@ -46,21 +48,23 @@ echo "** Steps 2-6: Stepwise optimization, ignoring latencies"
 # towards the middle of the code and repeat the process. The idea/hope is that
 # by doing this multiple times, the stalls will eventually be absorbed.
 
+# "Rule of thumb is that you should pick the split factor so that the window size is somewhere around 100"
+
 echo "*** Step 2"
 i=0
     ${SLOTHY_DIR}/slothy-cli Arm_AArch64 Arm_Cortex_A55                      \
        ${OPT_DIR}/bignum_mul_8_16_neon.objdump.nostack_unfold_process${i}.s            \
     -o ${OPT_DIR}/bignum_mul_8_16_neon.objdump.nostack_unfold_process$((${i}+1)).s     \
-    -c outputs=$OUTPUTS -c reserved_regs="[x18]"         \
+    -c outputs=$OUTPUTS -c reserved_regs="[x18,sp]"         \
     -c variable_size                                                         \
     -c max_solutions=512                                                     \
     -c timeout=300                                                           \
-    -c constraints.stalls_first_attempt=32                                   \
+    -c constraints.stalls_first_attempt=0                                   \
     -c split_heuristic                                                       \
     -c split_heuristic_region="[0,1]"                                        \
     -c objective_precision=0.1                                               \
     -c split_heuristic_stepsize=0.1                                          \
-    -c split_heuristic_factor=6                                              \
+    -c split_heuristic_factor=4                                              \
     -c constraints.model_latencies=False                                     \
     -c /visualize_reordering \
     $REDIRECT_OUTPUT
@@ -70,11 +74,11 @@ i=1
     ${SLOTHY_DIR}/slothy-cli Arm_AArch64 Arm_Cortex_A55                      \
        ${OPT_DIR}/bignum_mul_8_16_neon.objdump.nostack_unfold_process${i}.s            \
     -o ${OPT_DIR}/bignum_mul_8_16_neon.objdump.nostack_unfold_process$((${i}+1)).s     \
-    -c outputs=$OUTPUTS -c reserved_regs="[x18]"         \
+    -c outputs=$OUTPUTS -c reserved_regs="[x18,sp]"         \
     -c variable_size                                                         \
     -c max_solutions=512                                                     \
     -c timeout=180                                                           \
-    -c constraints.stalls_first_attempt=32                                   \
+    -c constraints.stalls_first_attempt=0                                   \
     -c split_heuristic                                                       \
     -c split_heuristic_region="[0,0.6]"                                      \
     -c objective_precision=0.1                                               \
@@ -90,17 +94,17 @@ i=2
     ${SLOTHY_DIR}/slothy-cli Arm_AArch64 Arm_Cortex_A55                      \
        ${OPT_DIR}/bignum_mul_8_16_neon.objdump.nostack_unfold_process${i}.s            \
     -o ${OPT_DIR}/bignum_mul_8_16_neon.objdump.nostack_unfold_process$((${i}+1)).s     \
-    -c outputs=$OUTPUTS -c reserved_regs="[x18]"         \
+    -c outputs=$OUTPUTS -c reserved_regs="[x18,sp]"         \
     -c variable_size                                                         \
     -c max_solutions=512                                                     \
     -c timeout=240                                                           \
-    -c constraints.stalls_first_attempt=32                                   \
+    -c constraints.stalls_first_attempt=0                                   \
     -c split_heuristic                                                       \
     -c split_heuristic_region="[0.3,1]"                                      \
     -c objective_precision=0.1                                               \
     -c constraints.move_stalls_to_top                                        \
     -c split_heuristic_stepsize=0.08                                         \
-    -c split_heuristic_factor=6                                              \
+    -c split_heuristic_factor=4                                              \
     -c split_heuristic_repeat=3                                              \
     -c constraints.model_latencies=False                                     \
     -c /visualize_reordering \
@@ -111,17 +115,17 @@ i=3
     ${SLOTHY_DIR}/slothy-cli Arm_AArch64 Arm_Cortex_A55                      \
        ${OPT_DIR}/bignum_mul_8_16_neon.objdump.nostack_unfold_process${i}.s            \
     -o ${OPT_DIR}/bignum_mul_8_16_neon.objdump.nostack_unfold_process$((${i}+1)).s     \
-    -c outputs=$OUTPUTS -c reserved_regs="[x18]"         \
+    -c outputs=$OUTPUTS -c reserved_regs="[x18,sp]"         \
     -c variable_size                                                         \
     -c max_solutions=512                                                     \
     -c timeout=240                                                           \
-    -c constraints.stalls_first_attempt=32                                   \
+    -c constraints.stalls_first_attempt=0                                   \
     -c split_heuristic                                                       \
     -c split_heuristic_region="[0.3,1]"                                      \
     -c objective_precision=0.1                                               \
     -c constraints.move_stalls_to_top                                        \
     -c split_heuristic_stepsize=0.05                                         \
-    -c split_heuristic_factor=5                                              \
+    -c split_heuristic_factor=4                                              \
     -c split_heuristic_repeat=3                                              \
     -c split_heuristic_abort_cycle_at=8                                      \
     -c constraints.model_latencies=False                                     \
@@ -133,22 +137,24 @@ i=4
     ${SLOTHY_DIR}/slothy-cli Arm_AArch64 Arm_Cortex_A55                      \
        ${OPT_DIR}/bignum_mul_8_16_neon.objdump.nostack_unfold_process${i}.s            \
     -o ${OPT_DIR}/bignum_mul_8_16_neon.objdump.nostack_unfold_process$((${i}+1)).s     \
-    -c outputs=$OUTPUTS -c reserved_regs="[x18]"         \
+    -c outputs=$OUTPUTS -c reserved_regs="[x18,sp]"         \
     -c variable_size                                                         \
     -c max_solutions=512                                                     \
     -c timeout=180                                                           \
-    -c constraints.stalls_first_attempt=32                                   \
+    -c constraints.stalls_first_attempt=0                                   \
     -c split_heuristic                                                       \
     -c split_heuristic_region="[0.2,1]"                                      \
     -c objective_precision=0.1                                               \
     -c constraints.move_stalls_to_top                                        \
     -c split_heuristic_stepsize=0.05                                         \
-    -c split_heuristic_factor=5                                              \
+    -c split_heuristic_factor=4                                              \
     -c split_heuristic_repeat=3                                              \
     -c split_heuristic_abort_cycle_at=5                                      \
     -c constraints.model_latencies=False                                     \
     -c /visualize_reordering \
     $REDIRECT_OUTPUT
+
+fi
 
 # Finally, also consider latencies
 
@@ -158,17 +164,17 @@ i=5
    ${SLOTHY_DIR}/slothy-cli Arm_AArch64 Arm_Cortex_A55                       \
        ${OPT_DIR}/bignum_mul_8_16_neon.objdump.nostack_unfold_process${i}.s            \
     -o ${OPT_DIR}/bignum_mul_8_16_neon.objdump.nostack_unfold_process$((${i}+1)).s     \
-    -c outputs=$OUTPUTS -c reserved_regs="[x18]"         \
+    -c outputs=$OUTPUTS -c reserved_regs="[x18,sp]"         \
     -c variable_size                                                         \
     -c max_solutions=512                                                     \
     -c timeout=300                                                           \
-    -c constraints.stalls_first_attempt=32                                   \
+    -c constraints.stalls_first_attempt=0                                   \
     -c split_heuristic                                                       \
     -c split_heuristic_region="[0,1]"                                        \
     -c objective_precision=0.1                                               \
     -c split_heuristic_stepsize=0.05                                         \
     -c split_heuristic_optimize_seam=10                                      \
-    -c split_heuristic_factor=8                                              \
+    -c split_heuristic_factor=10                                              \
     -c split_heuristic_repeat=10                                             \
     -c /visualize_reordering \
     $REDIRECT_OUTPUT
@@ -178,17 +184,17 @@ i=6
   ${SLOTHY_DIR}/slothy-cli Arm_AArch64 Arm_Cortex_A55                       \
       ${OPT_DIR}/bignum_mul_8_16_neon.objdump.nostack_unfold_process${i}.s         \
    -o ${OPT_DIR}/bignum_mul_8_16_neon.objdump.nostack_unfold_process$((${i}+1)).s  \
-    -c outputs=$OUTPUTS -c reserved_regs="[x18]"         \
+    -c outputs=$OUTPUTS -c reserved_regs="[x18,sp]"         \
    -c variable_size                                                         \
    -c max_solutions=512                                                     \
    -c timeout=300                                                           \
-   -c constraints.stalls_first_attempt=32                                   \
+   -c constraints.stalls_first_attempt=0                                   \
    -c split_heuristic                                                       \
    -c split_heuristic_region="[0,1]"                                        \
    -c objective_precision=0.1                                               \
    -c split_heuristic_stepsize=0.05                                         \
    -c split_heuristic_optimize_seam=10                                      \
-   -c split_heuristic_factor=8                                              \
+   -c split_heuristic_factor=10                                              \
    -c split_heuristic_repeat=3                                              \
    -c /visualize_reordering \
     $REDIRECT_OUTPUT
@@ -198,17 +204,17 @@ i=7
   ${SLOTHY_DIR}/slothy-cli Arm_AArch64 Arm_Cortex_A55                       \
       ${OPT_DIR}/bignum_mul_8_16_neon.objdump.nostack_unfold_process${i}.s         \
    -o ${OPT_DIR}/bignum_mul_8_16_neon.objdump.nostack_opt.s                           \
-    -c outputs=$OUTPUTS -c reserved_regs="[x18]"         \
+    -c outputs=$OUTPUTS -c reserved_regs="[x18,sp]"         \
    -c variable_size                                                         \
    -c max_solutions=512                                                     \
    -c timeout=300                                                           \
-   -c constraints.stalls_first_attempt=32                                   \
+   -c constraints.stalls_first_attempt=0                                   \
    -c split_heuristic                                                       \
    -c split_heuristic_region="[0,1]"                                        \
    -c objective_precision=0.1                                               \
    -c split_heuristic_stepsize=0.05                                         \
    -c split_heuristic_optimize_seam=10                                      \
-   -c split_heuristic_factor=8                                              \
+   -c split_heuristic_factor=10                                              \
    -c split_heuristic_repeat=3                                              \
    -c constraints.move_stalls_to_top                                        \
    -c /visualize_reordering \
